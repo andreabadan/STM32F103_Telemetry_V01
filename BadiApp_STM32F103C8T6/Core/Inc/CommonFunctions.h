e21ed9a6fa1d24a8ad4ed3657aaaf90779223833
andreabadan <45894591+andreabadan@users.noreply.github.com>
2021-12-15 23:37:03 +0100

    Improved filter calculation

--- BadiApp_STM32F103C8T6/Core/Inc/CommonFunctions.h
+++ BadiApp_STM32F103C8T6/Core/Inc/CommonFunctions.h
@@ -12,11 +12,7 @@ typedef struct{
 	float In;   // Input
 	float R;    // variance of the noise on the measurement
 	float Q;    // variance of the disturbance on the process
-	float Pp;   // P (t | t-1) variance of the prediction error
-	float K;    // Kalman gain
-	float e;    // Epsilon, term of innovation
 	float P;    // P (t | t) variance of the filtering error
-	float Xp;   // x_^ (t | t-1) forecast of the previous state
 	float Out;  // Output
 }KalmanFilterStruct;
 
--- BadiApp_STM32F103C8T6/Core/Src/CommonFunctions.c
+++ BadiApp_STM32F103C8T6/Core/Src/CommonFunctions.c
@@ -9,10 +9,13 @@
 
 //KalmanFilter
 void KalmanFilter(KalmanFilterStruct *Struct){
-	Struct->Pp = Struct->P + Struct->Q;                // (PREDICTION) prediction of the variance of error in the next step
-	Struct->Xp = Struct->Out;                          // (PREDICTION) state prediction in the next step
-	Struct->K  = Struct->Pp/(Struct->Pp + Struct->R);  // (CORRECTION) Kalman gain
-	Struct->e  = Struct->In-Struct->Xp;                // (CORRECTION) innovation update
-	Struct->P  = (1-Struct->K)*Struct->Pp;             // (CORRECTION) updating the variance of the filtering error
-	Struct->Out = Struct->Xp + Struct->K*Struct->e;    // (CORRECTION) output
+	//P (t | t-1) variance of the prediction error
+	float Pp = Struct->P + Struct->Q;                // (PREDICTION) prediction of the variance of error in the next step
+	//x_^ (t | t-1) forecast of the previous state
+	float Xp = Struct->Out;                          // (PREDICTION) state prediction in the next step
+	//Kalman gain
+	float K  = Pp/(Pp + Struct->R);                  // (CORRECTION) Kalman gain
+
+	Struct->P  = (1-K)*Pp;                           // (CORRECTION) updating the variance of the filtering error
+	Struct->Out = Xp + K*(Struct->In-Xp);            // (CORRECTION) output + Kalman gain * variance of the filtering error
 }
