40695d8d0e4b79036abf4114c2891aebc4b2e75b
andreabadan <45894591+andreabadan@users.noreply.github.com>
2022-06-03 18:50:08 +0200

    Updated new path

--- writeBoot.bat
+++ writeBoot.bat
@@ -4,6 +4,6 @@ echo (on standard path C:\Program Files\STMicroelectronics\STM32Cube\STM32CubePr
 C:
 
 cd C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin
-STM32_Programmer_CLI.exe -c port=COM4 -e 0 19 -w %~dp0\Bootloader_STM32F103C8T6\Debug\Bootloader_STM32F103C8T6.bin 0x08000000
+STM32_Programmer_CLI.exe -c port=COM4 -e 0 19 -w %~dp0\Bootloader_STM32F103CBT6\Debug\Bootloader_STM32F103CBT6.bin 0x08000000
 
 @pause
