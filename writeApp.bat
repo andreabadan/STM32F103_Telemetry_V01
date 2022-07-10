e45e843b3efdd578315b8c20f861c25f8496175e
andreabadan <45894591+andreabadan@users.noreply.github.com>
2022-07-10 23:49:43 +0200

    Fix auto upload

--- writeApp.bat
+++ writeApp.bat
@@ -4,6 +4,6 @@ echo(on standard path C:\Program Files\STMicroelectronics\STM32Cube\STM32CubePro
 
 C:
 cd C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin
-STM32_Programmer_CLI.exe -c port=COM4 -e 20 -w %~dp0\BadiApp_STM32F103CBT6\Debug\BadiApp_STM32F103CBT6.bin 0x08005000
+STM32_Programmer_CLI.exe -c port=COM5 -e 20 -w %~dp0\BadiApp_STM32F103CBT6\Debug\BadiApp_STM32F103CBT6.bin 0x08005000
 
 @pause
--- writeBoot.bat
+++ writeBoot.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Writing Application using STM32_Programmer_CLI.exe 
-echo (on standard path C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin)
-C:
+echo(on standard path C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin)
 
+C:
 cd C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin
-STM32_Programmer_CLI.exe -c port=COM4 -e 0 19 -w %~dp0\Bootloader_STM32F103CBT6\Debug\Bootloader_STM32F103CBT6.bin 0x08000000
+STM32_Programmer_CLI.exe -c port=COM5 -e 0 19 -w %~dp0\Bootloader_STM32F103CBT6\Debug\Bootloader_STM32F103CBT6.bin 0x08000000
 
 @pause
